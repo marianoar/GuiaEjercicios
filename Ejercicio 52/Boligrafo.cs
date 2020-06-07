5d4156e6dbad25eab1eea26721726729055d8f0d
marianoar <marianow2006@yahoo.com.ar>
2020-06-06 21:13:41 -0300

    ej 52

---
+++ Ejercicio 52/Boligrafo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_52
+{
+    public class Boligrafo : IAcciones
+    {
+
+        private ConsoleColor color;
+        private float tinta;
+
+        public Boligrafo(float tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor Color
+        {
+            get { return this.color; }
+            set { throw new NotImplementedException(); }
+        }
+        public float UnidadesDeEscritura
+        {
+            get { return tinta; }
+            set { }
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            for (int i = 0; i < texto.Length; i++)
+            {
+                tinta -= 0.3F;
+            }
+            return new EscrituraWrapper(texto, Color); 
+        }
+
+        public bool Recargar(int unidades)
+        {
+            tinta += unidades;
+           return true;
+        }
+
+        public override string ToString()
+        {
+            return "Esto es un Boligrafo de color "+ Color.ToString()+ " con  nivel de tinta en "+tinta.ToString();
+        }
+    }
+}
---
+++ Ejercicio 52/Lapiz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_52
+{
+    public class Lapiz : IAcciones
+    {
+        private float tamanioMina;
+
+        public  ConsoleColor Color
+        {
+            get { return ConsoleColor.Red; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get { return tamanioMina; }
+            set { }
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            float reducir = texto.Length * (float)0.1;
+
+            tamanioMina = tamanioMina - reducir;
+            
+            
+            return new EscrituraWrapper(texto, Color);
+
+        }
+        public Lapiz(int unidades)
+        {
+            tamanioMina = unidades;
+        }
+
+        public bool Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+  
+        }
+
+        public override string ToString()
+        {
+            return "Esto es un Lapiz de color " + Color.ToString() + " con tamanio mina en " + tamanioMina.ToString();
+        }
+
+    }
+}
