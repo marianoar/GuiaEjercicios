f99605337393d1a3ce0dab63cdb85a759a907c76
marianoar <marianow2006@yahoo.com.ar>
2020-06-12 23:48:58 -0300

    avances ej 5*

---
+++ Ejercicio 58/Archivo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_56;
+
+namespace Ejercicio_58
+{
+    public abstract class Archivo
+    {
+
+        
+    }
+}
