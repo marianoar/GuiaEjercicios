4c97b22ee6db8e3e626a47fb8f48553adfe82566
marianoar <marianow2006@yahoo.com.ar>
2020-06-27 01:08:05 -0300

    Ejercicio 63

---
+++ Ejercicio 63/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Ejercicio_63
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtb.Location = new System.Drawing.Point(0, 28);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(800, 422);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(376, 9);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(35, 13);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Label lblHora;
+    }
+}
+
---
+++ Ejercicio 63/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_63
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        DateTime time = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Ejercicio 63";
+            lblHora.Text = time.ToString();
+            thread = new Thread(new ParameterizedThreadStart(MostrarTiempo));
+            thread.Start();
+        }
+
+        public void MostrarTiempo(object obj)
+        {
+
+            while (true)
+            {
+                if (lblHora.InvokeRequired)
+                {
+                    lblHora.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        lblHora.Text = DateTime.Now.ToString("dd/M/yyyy hh:MM:ss");
+                    });
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+        }
+    }
+}
